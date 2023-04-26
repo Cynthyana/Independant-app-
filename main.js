194ca87eef8b19cbabb62785d6a65667e0cbf679
Cynthyana <96613566+Cynthyana@users.noreply.github.com>
2023-04-26 18:32:02 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function addText2(){
+    var dates= document.getElementById("date").value;
+document.getElementById("p2").innerHTML= dates;
+}
+
+function addText1(){
+    var rems= document.getElementById("rem").value;
+document.getElementById("p1").innerHTML= rems;
+}
